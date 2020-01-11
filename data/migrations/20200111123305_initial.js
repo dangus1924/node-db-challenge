695dc3e72412d1a051272e2fa5529482db9d0882
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-11 13:03:25 -0500

    created migration for 4 tables

---
+++ data/migrations/20200111123305_initial.js
@@ -0,0 +1,54 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable('projects', (table) => {
+      table.increments('id')
+      table.string('name')
+        .notNullable()
+        .unique()
+      table.text('description')
+      table.boolean('completed').defaultTo(0)
+  })
+
+  await knex.schema.createTable('resources', (table) => {
+    table.increments('id')
+    table.string('name')
+        .notNullable()
+        .unique()
+    table.string('description')
+
+  })
+
+  await knex.schema.createTable('tasks', (table) => {
+    table.increments('id')
+    table.string('name')
+        .notNullable()
+        .unique()
+    table.text('notes')
+    table.string('description')
+    table.boolean('completed').defaultTo(0)
+    table.integer('projects_id')
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+
+  })
+  await knex.schema.createTable('project_resources', (table) => {
+    table.integer('projects_id')
+        .notNullable()
+        .references('id')
+        .inTable('projects')
+    table.integer('resources_id')
+        .notNullable()
+        .references('id')
+        .inTable('resources')
+    table.primary(['projects_id', 'resources_id'])
+  })
+
+};
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('projet_resources')
+    await knex.schema.dropTableIfExists('tasks')
+    await knex.schema.dropTableIfExists('resources')
+    await knex.schema.dropTableIfExists('projects')
+};
