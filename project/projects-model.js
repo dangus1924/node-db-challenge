e1dc48173665216608ef40e8ff3ef457e2b4f772
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-11 13:32:55 -0500

    created first model

--- project/projects-model.js
+++ project/projects-model.js
@@ -1,7 +1,13 @@
 const db = require('../data/db-config')
 
 async function getReources(project_id) {
-
+    const resources = await db("projects_resources as p_r")
+    .join("projects as p", "p_r.projects_id", "p.id")
+    .join("resources as r", "p_r.resources_id", "r.id")
+    .select("p.name", "r.name")
+    .where("p.id", project_id)
+    .first()
+return resources
 }
 async function addResource() {
     
