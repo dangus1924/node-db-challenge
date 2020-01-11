b7fdb4d4b882052ef8534a7a53b249f64eef22a2
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-11 13:47:16 -0500

    imported the porject router into the index file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const helmet = require('helmet')
+const projectRouter = require('./project/projects-router')
 
 const server = express()
 const port = process.env.PORT || 4001
@@ -11,6 +12,14 @@ server.get('/', (req, res, next) => {
     res.send('<h1>Welcome to Business API</h1>')
 })
 
+server.use('/api', projectRouter)
+
+server.use((err, req, res, next) => {
+    console.log('Error:', err)
+    res.status(500).json({
+        message: "Oh Man... It's not you it's me. I swear!"
+    })
+})
 
 server.listen(port, () => {
     console.log(`Listening on port:${port}`)
